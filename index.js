36f9e696ced00cb78f2286e72b86125fd33280c2
ChristoherHenao <cq.henao@gmail.com>
2021-04-14 16:52:49 -0400

    6/7 tasks solved

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ Use the addFlavor function below to do the following:
 
     For example: addFlavor(originalFlavors, "Rainbow Sherbert") should return the array ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"]
 */
-function addFlavor(array,newFlavor){
+function addFlavor(array, newFlavor){
    array.unshift(newFlavor);
    return array;
 }
@@ -113,9 +113,8 @@ Use the getFlavorByIndex function below to do the following:
 
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
-
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(array, index){
+    return array[index];
 }
 
 
@@ -134,9 +133,19 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(array, flavor){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === flavor){
+            array.splice(i,1);
+            return array;
+        }    
+        
+    
+    }
+
 }
+// console.log(removeFlavorByName(originalFlavors,"Coffee"));
+// console.log(originalFlavors);
 
 
 
